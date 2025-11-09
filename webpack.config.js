a4b32d3eee88781e72e37b4b34edc7186ce0a018
Carlos Enrique Hernandez Hernandez <hernandez.enrique.carlos@gmail.com>
2025-11-09 17:15:42 -0600

    ci: agrego bandera de publicpath

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ export default {
   output: {
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/who-am-i/',
     clean: true,
   },
   module: {
