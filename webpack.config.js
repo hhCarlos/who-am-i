66affd262e10bc26d5a63f3d431e47308f53abf3
Carlos Enrique Hernandez Hernandez <hernandez.enrique.carlos@gmail.com>
2025-11-09 21:03:10 -0600

    ci: s3 integracion con secrets y cambios en webpack

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,6 @@ export default {
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
-      publicPath: '/who-am-i/', // 👈 también aquí por si acaso
     }),
     new MiniCssExtractPlugin({
       filename: 'styles.[contenthash].css',
